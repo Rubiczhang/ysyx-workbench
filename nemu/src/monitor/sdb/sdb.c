d330bd7f016fc9eace145e6f59aaff71f14807d5
tracer-ysyx <tracer@ysyx.org>
2024-08-06 07:06:09 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:06:09 up 4 days, 13:25,  5 users,  load average: 0.08, 0.06, 0.15

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -57,7 +57,7 @@ static int cmd_si(char *args) ;
 
 static int cmd_info(char *args){
   char *arg = strtok(NULL, " ");
-  if(!strcmp(arg, "r")){
+  if(arg && !strcmp(arg, "r")){
     isa_reg_display();
   }
   return 0;
