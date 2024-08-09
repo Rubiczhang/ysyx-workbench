a9fdd214679ecd18edaa7ab2b57633032e3cc30b
tracer-ysyx <tracer@ysyx.org>
2024-08-09 16:23:39 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:39 up  1:07,  3 users,  load average: 0.14, 0.15, 0.09

--- nemu/src/test/expr_ut.c
+++ nemu/src/test/expr_ut.c
@@ -1,5 +1,15 @@
 #include <common.h>
 
+char * test[] = {" + ==  == +   "};
+
+void expr(char* e, bool* success);
+
 void expr_ut(void){
-    printf("Print from expr_ut\n");
+    bool succ;
+    for(int i = 0; i < sizeof(test)/sizeof(char*); i++){
+        expr(test[i], &succ);
+        if(succ){
+            printf("Passed expr: %s\n", test[i]);
+        }
+    }
 }
