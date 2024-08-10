94fedcf1974fd76f3d4d8be6bf2ff469bbea761f
tracer-ysyx <tracer@ysyx.org>
2024-08-10 18:02:18 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:02:18 up 1 day,  2:46,  3 users,  load average: 0.12, 0.13, 0.10

--- nemu/src/test/expr_ut.c
+++ nemu/src/test/expr_ut.c
@@ -9,12 +9,12 @@ char * test[] = {" + ==  == +   ",
 
 char *test1[] = {
             "1+2*3-5",//2
-            "-1+2*3-5",//2
-            "(-1   + 2)- 3    -5",//-7
-            "(-1+2)*3-5",//-2
-            "-(-1+2)*3-5",//-8
-            "-((-1+2)*3)-5",//--8
-            "--1" //1
+            // "-1+2*3-5",//2
+            // "(-1   + 2)- 3    -5",//-7
+            // "(-1+2)*3-5",//-2
+            // "-(-1+2)*3-5",//-8
+            // "-((-1+2)*3)-5",//--8
+            // "--1" //1
 };
 
 word_t expr(char* e, bool* success);
@@ -25,13 +25,13 @@ bool make_token(char *e) ;
 
 void expr_ut(void){
     bool succ;
-    for(int i = 0; i < sizeof(test)/sizeof(char*); i++){
-        // getMainOptr_UT(test1[i], &succ);
-        word_t val = expr(test[i], &succ);
-        if(succ){
-            printf("Get value: %d\n", val);
-        }
-    }
+    // for(int i = 0; i < sizeof(test)/sizeof(char*); i++){
+    //     // getMainOptr_UT(test1[i], &succ);
+    //     word_t val = expr(test[i], &succ);
+    //     if(succ){
+    //         printf("Get value: %d\n", val);
+    //     }
+    // }
     for(int i = 0; i < sizeof(test1)/sizeof(char*); i++){
             // getMainOptr_UT(test1[i], &succ);
             word_t val = expr(test1[i], &succ);
