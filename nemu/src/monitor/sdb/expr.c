4dae15234a49d5508165ac808eb12dda49d577de
tracer-ysyx <tracer@ysyx.org>
2024-08-10 12:43:18 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:43:18 up 21:27,  3 users,  load average: 0.19, 0.10, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -14,11 +14,13 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include <stdbool.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
+
 
 enum {
   TK_NOTYPE = 256, 
@@ -78,6 +80,9 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
+
+static bool isSingleOperator(Token token);
+
 static bool make_token(char *e) {
   int position = 0;
   int i;
@@ -108,9 +113,9 @@ static bool make_token(char *e) {
             break;
           default:
             strncpy(tokens[nr_token++].str, substr_start, substr_len);
-#ifdef UT
-  Log("added token: #%d: %.*s", nr_token-1, substr_len, substr_start);
-#endif
+          #ifdef UT
+            Log("added token: #%d: %.*s", nr_token-1, substr_len, substr_start);
+          #endif
         }
         break;
       }
@@ -126,17 +131,191 @@ static bool make_token(char *e) {
 }
 
 
+static void print_tokens(Token* tokens, int32_t beg, int32_t end ){
+  Log("Illegal expression: ");
+  for(int i = beg; i < end; i++){
+    printf("%s ", tokens[i].str);
+  }
+  printf("\n");
+}
+
+static int32_t getEndOfParnth(Token* tokens, int beg, int end){
+  assert(tokens[beg].type == '(');
+  int cnt_left_prntth = 1;
+  int i = 0;
+  for(i = beg; i <= end; i++){
+    if(tokens[i].type == '(')
+      cnt_left_prntth ++;
+    else if(tokens[i].type == ')')
+      cnt_left_prntth--;
+    if(cnt_left_prntth == 0){
+      return i;
+    }
+  }
+  return -1;
+}
+
+// static int32_t getEndOfFirstExpr(Token* tokens, int beg, int end){
+//   //return beg if fisrt token is decimal int
+//   //return pos of corrosponding ')' if first token is '('
+//   assert( 
+//           tokens[beg].type == TK_DINT ||
+//           tokens[beg].type == '(' ||
+//           isSingleOperator(tokens[beg])
+//   );
+//   if(tokens[beg].type == TK_DINT){
+//     return beg;
+//   }
+//   else if(tokens[beg].type == '('){
+//     return getEndOfParnth(tokens, beg, end);
+//   }
+//   else if(isSingleOperator(tokens[beg])){
+//     return getEndOfFirstExpr(tokens, beg+1, end);
+//   }
+// }
+
+static bool isBinOperator(Token token){
+  bool res = false;
+  switch(token.type){
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case TK_EQ:
+      res = true;
+      break;
+  }
+  return res;
+  
+}
+static bool isSingleOperator(Token token){
+  bool res = false;
+  switch(token.type){
+    case '-':
+    case '*':
+      res = true;
+      break;
+  }
+  return res;
+}
+
+static word_t getBinOprValue(word_t first, Token op_tk, word_t last){
+  Assert(isBinOperator(op_tk), "Op is not binary operater, Op:%s\n", op_tk.str);
+
+  word_t val = 0;
+  switch(op_tk.type){
+    case '+':
+      val = first+last;
+      break;
+    case '-':
+      val = first-last;
+      break;
+    case '*':
+      val = first*last;
+      break;
+    case '/':
+      val = first/last;
+      break;
+    case TK_EQ :
+      val = first == last;
+      break;
+  }
+  return val;
+}
+
+static word_t  getSigOprValue(Token op_tk, word_t oprnd_val){
+  assert(isSingleOperator(op_tk));
+  word_t val;
+  
+  switch(op_tk.type){
+    case '-':
+      val = - oprnd_val;
+      break;
+    // case '*':
+    //   val = *(word_t*)oprnd_val;
+    //   break;
+  }
+  return val;
+}
+
+static int32_t prcdcOprtr(Token op_tk){
+  assert(isBinOperator(op_tk) | isSingleOperator(op_tk));
+  switch(op_tk.type){
+    case '+':
+    case '-':   //same precedence for sub and neg Operator
+      return 1;
+    case '*':   //same precedence for ptr and mul Operator
+    case '/':
+      return 2;
+  }
+  Assert(0, "Inner Error: get precendence of token: %s\n", op_tk.str);
+  return 0xffff;
+}
+
+static int32_t getMainOprtr(Token* tokens, int beg, int end){
+  int32_t mainOprtPos = -1;
+  int32_t mainOptrPrcdc = 0xffff;
+  for(int i = beg; i < end; ){
+    if(tokens[i].type == '(' ){
+      i = getEndOfParnth(tokens, i, end);
+      if(i == -1)
+        print_tokens(tokens, beg, end);
+      continue;
+    }
+    if(isBinOperator(tokens[i]) || isSingleOperator(tokens[i])){
+      if(prcdcOprtr(tokens[i]) <= mainOptrPrcdc){
+        mainOprtPos = i;
+      }
+    }
+  }
+
+  return mainOprtPos;
+  
+}
+
+static word_t eval(Token* tokens, int beg, int end){
+  word_t value = 1;
+  Assert(beg <= end, "input of eval is illegal: beg:%d, end:%d\n",beg, end);
+  return value;
+  if(beg == end){           //<number>
+    assert(tokens[beg].type == TK_DINT);
+    value = strtol(tokens[beg].str, NULL, 10);
+  }
+  else if(tokens[beg].type == '(' && (getEndOfParnth(tokens, beg, end) == end)){
+    // '(' <expr> ')'
+    value = eval(tokens, beg+1, end-1);
+  }
+  else{
+    int32_t mainOptrPos = getMainOprtr(tokens, beg, end);
+    if(mainOptrPos >= end){
+      print_tokens(tokens, beg, end);
+      return value;
+    }
+    
+    word_t leftValue;
+    if(mainOptrPos > beg)
+      leftValue = eval(tokens, beg, mainOptrPos-1);
+
+    Token op_token = tokens[mainOptrPos];
+    // assert(isBinOperator(op_token));
+
+    word_t rightValue = eval(tokens, mainOptrPos+1, end);
+    
+    if(mainOptrPos > beg)
+      value = getBinOprValue(leftValue, op_token, rightValue);
+    else
+      value = getSigOprValue(op_token, rightValue);
+  }
+  return value;
+
+}
+
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-#ifdef UT
-  *success = true;
-  return 0;
-#endif
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
+  eval(tokens, 0, nr_token);
   return 0;
 }
--- nemu/src/test/expr_ut.c
+++ nemu/src/test/expr_ut.c
@@ -2,18 +2,22 @@
 
 char * test[] = {" + ==  == +   ",
                 " +== ==+ ",
-                " -1100+124321 1",
-                " -1100*+/124321 1",
-                " -1100*(124+ ==)+/124321 1"
+                // " -1100+124321 1",
+                // " -1100*+/124321 1",
+                // " -1100*(124+ ==)+/124321 1"
                 };
 
+char *test1[] = {
+            "1+2*3-5",//2
+            "1+2*3-5"//2
+};
 
-void expr(char* e, bool* success);
+word_t expr(char* e, bool* success);
 
 void expr_ut(void){
     bool succ;
     for(int i = 0; i < sizeof(test)/sizeof(char*); i++){
-        expr(test[i], &succ);
+        expr(test1[i], &succ);
         if(succ){
             printf("Passed expr: %s\n", test[i]);
         }
