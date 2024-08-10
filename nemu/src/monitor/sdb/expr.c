063ea6097217798a88bec37c650c4b2707e94e1e
tracer-ysyx <tracer@ysyx.org>
2024-08-10 17:18:02 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:18:02 up 1 day,  2:01,  3 users,  load average: 0.24, 0.14, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -286,6 +286,8 @@ static int32_t getMainOprtr(Token* tokens, int beg, int end){
     else {
       
       if(isBinOperator(tokens[i]) || isSingleOperator(tokens[i])){
+        printf("i: %d, tokens[i].str: %s, isLastNonSpaceTkEndOfExpr: %d\n", 
+                i, tokens[i].str, isLastNonSpaceTkEndOfExpr);
         bool isSingle = !isLastNonSpaceTkEndOfExpr;
         if(prcdcOprtr(tokens[i], isSingle) <= mainOptrPrcdc){
           mainOprtPos = i;
@@ -298,6 +300,7 @@ static int32_t getMainOprtr(Token* tokens, int beg, int end){
   }
   print_tokens(tokens, beg, end);
   printf("mainOprtPos: %d\n", mainOprtPos);
+  
 
   return mainOprtPos;
   
