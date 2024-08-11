0239d3c3c4e35e05cbe2e2f83767a827d3ced23b
tracer-ysyx <tracer@ysyx.org>
2024-08-11 14:50:44 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:50:44 up 1 day, 23:34,  3 users,  load average: 0.32, 0.16, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -128,6 +128,8 @@ static bool make_token(char *e) {
 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      char ch = e[position];
+      printf("%c", ch);
       return false;
     }
   }
