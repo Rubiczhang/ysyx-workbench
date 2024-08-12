caab43c88ba8e0ed292f45682bbc0f46153dd14a
tracer-ysyx <tracer@ysyx.org>
2024-08-12 14:25:09 +0800

    > compile NEMU 100006534 张东宇 Linux DESKTOP-5R7SL4V 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:25:09 up 2 days, 23:09,  5 users,  load average: 0.16, 0.07, 0.08

--- nemu/src/test/expr_ut.c
+++ nemu/src/test/expr_ut.c
@@ -53,23 +53,23 @@ void expr_ut(void){
             printf("%d", buff[strlen(buff)-1]);
             Log("Wrong input:%s", buff);
         }
-        assert(buff[strlen(buff)-1] == '\n'  || //input from file
-                    buff[strlen(buff)-1] == EOF ); //input from end line of file
-        buff[strlen(buff)-1] = '\0';
-        sscanf(buff, "%s", ans_buf);
-        word_t ans = strtol(ans_buf, NULL, 10);
-        bool success = false;
-        word_t eval_ans = expr(buff+strlen(ans_buf), &success);
-        if(success){
-            FILE* fp = fopen("wrong_expr.txt", "w");
-            if(fp){
-                fprintf(fp, "%s", buff);
-                fclose(fp);
-            }
-            Assert(ans == eval_ans, "Wrong Ans: %u vs Right Ans %u\n expression:%s\n", eval_ans, ans, buff+strlen(ans_buf));
-            Log("PASSED, Expression: %s", buff+strlen(ans_buf));
-        } else{
-            Log("Something Wrong Happend, Expression:%s", buff+ strlen(ans_buf));
-        }
+//         assert(buff[strlen(buff)-1] == '\n'  || //input from file
+//                     buff[strlen(buff)-1] == EOF ); //input from end line of file
+//         buff[strlen(buff)-1] = '\0';
+//         sscanf(buff, "%s", ans_buf);
+//         word_t ans = strtol(ans_buf, NULL, 10);
+//         bool success = false;
+//         word_t eval_ans = expr(buff+strlen(ans_buf), &success);
+//         if(success){
+//             FILE* fp = fopen("wrong_expr.txt", "w");
+//             if(fp){
+//                 fprintf(fp, "%s", buff);
+//                 fclose(fp);
+//             }
+//             Assert(ans == eval_ans, "Wrong Ans: %u vs Right Ans %u\n expression:%s\n", eval_ans, ans, buff+strlen(ans_buf));
+//             Log("PASSED, Expression: %s", buff+strlen(ans_buf));
+//         } else{
+//             Log("Something Wrong Happend, Expression:%s", buff+ strlen(ans_buf));
+//         }
     }
 }
