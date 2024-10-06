53b0ca12f62fa1e8f6b8f87a536e41b2f2ea855d
zhangdongyu <zdy666_@outlook.com>
2024-10-06 12:13:31 +0800

    Add difftest for nemu

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,16 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool ret = true;
+  for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++){
+    if(cpu.gpr[i] != ref_r->gpr[i]){
+      ret = false;
+      break;
+    }
+  }
+  if(cpu.pc != ref_r->pc)
+    ret = false;
+  return ret ;
 }
 
 void isa_difftest_attach() {
